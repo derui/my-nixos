0651123dd068e94b6350bc36b37f0d7a433772bc
derui <derutakayu@gmail.com>
2024-10-05 15:17:43 +0900

    fix: enable hyprland on home-manager

--- dotfiles/hyprland.nix
+++ dotfiles/hyprland.nix
@@ -1,5 +1,9 @@
 {...}:
 {
+  wayland.windowManager.hyprland = {
+    enable = true;
+  };
+
   wayland.windowManager.hyprland.settings = {
     "$mod" = "SUPER";
     "$terminal" = "kitty";
@@ -13,7 +17,7 @@
     input = {
       kb_layout = "us";
       follow_mouse = 1;
-      sensitivity = 0
+      sensitivity = 0;
     };
 
     general = {
@@ -55,12 +59,14 @@
     
         bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
     
-        animation = "windows, 1, 7, myBezier";
-        animation = "windowsOut, 1, 7, default, popin 80%";
-        animation = "border, 1, 10, default";
-        animation = "borderangle, 1, 8, default";
-        animation = "fade, 1, 7, default";
-        animation = "workspaces, 1, 2, default";
+        animation = [
+          "windows, 1, 7, myBezier"
+        "windowsOut, 1, 7, default, popin 80%"
+        "border, 1, 10, default"
+        "borderangle, 1, 8, default"
+        "fade, 1, 7, default"
+        "workspaces, 1, 2, default"
+        ];
     };
 
     dwindle = {
@@ -79,8 +85,10 @@
         force_default_wallpaper = -1; # Set to 0 or 1 to disable the anime mascot wallpapers
     };
     
-    windowrulev2 = "suppressevent maximize, class:.*"; # You'll probably like this.
-    windowrulev2 = "immediate, class:^(cs2)$";
+    windowrulev2 =[
+      "suppressevent maximize, class:.*" # You'll probably like this.
+     "immediate, class:^(cs2)$"
+     ];
 
     bind =
       [
