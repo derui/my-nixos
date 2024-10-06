4d01aca12513c02e4565bb7daecda47c4f3f68d8
derui <derutakayu@gmail.com>
2024-10-06 13:07:33 +0900

    fix: method to override

--- home-manager/gui/emacs/default.nix
+++ home-manager/gui/emacs/default.nix
@@ -1,9 +1,21 @@
-{ pkgs, home, ... }:
+{ pkgs, home, epkgs, ... }:
 {
   # lspを高速化するための拡張
   home.packages = with pkgs; [ emacs-lsp-booster ];
 
   programs.emacs = {
-    enable = true;
+    # enableするとstableのが入ってくるので、home.packageに任せる
+    extraPackages = (epkgs: with epkgs; [
+      magit
+    ]);
+
+    overrides = self: super: rec {
+      magit = self.melpaPackages.magit.override (args: {
+        melpaBuild = drv: args.melpaBuild (drv // {
+          commit = "4d054196eb1e99251012fc8b133db9512d644bf1";
+        });
+
+      });
+    };
   };
 }
