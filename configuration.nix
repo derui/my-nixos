8066fb8123868a1e95669135ce032e62c22eda93
derui <derutakayu@gmail.com>
2024-10-12 10:54:34 +0900

    fix: invalid script

--- configuration.nix
+++ configuration.nix
@@ -154,7 +154,7 @@
       ''
         mkdir -m 0755 -p /bin
         ln -sfn ${pkgs.bash}/bin/bash /bin/.bash.tmp
-        mv /bin/.bash.tmp /usr/bash
+        mv /bin/.bash.tmp /bin/bash
       '';
   };
 
