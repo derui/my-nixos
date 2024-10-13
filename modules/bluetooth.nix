62d5e0397733018e7daf5bf519448e219b298ba9
derui <derutakayu@gmail.com>
2024-10-13 18:50:42 +0900

    fix: add some options for bluetooth

--- modules/bluetooth.nix
+++ modules/bluetooth.nix
@@ -9,6 +9,16 @@
         # for low-energy support
         Privacy = "device";
         ControllerMode = "dual";
+        UserspaceHID = true;
+        ClassicBondedOnly = false;
+        LEAutoSecurity = false;
+      };
+
+      LE = {
+        # for xpadneo recommendation
+        MinConnectionInterval = 7;
+        MaxConnectionInterval = 9;
+        ConnectionLatency = 0;
       };
 
     };
