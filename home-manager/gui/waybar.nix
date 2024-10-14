75909b0f6e7136e59eeebf1f9ce2f1907515c37f
derui <derutakayu@gmail.com>
2024-10-14 13:26:29 +0900

    refactor: simplify config file installation

--- home-manager/gui/waybar.nix
+++ home-manager/gui/waybar.nix
@@ -1,11 +1,8 @@
-{ home, config, ... }:
+{ ... }:
 {
   # waybarの設定は結構独自なので、自分で設定する
-  home.file."${config.xdg.configHome}/waybar/config" = {
-    source = ./waybar.conf;
-  };
-
-  home.file."${config.xdg.configHome}/waybar/style.css" = {
-    source = ./waybar.style.css;
+  xdg.configFile = {
+    "waybar/config".source = ./waybar.conf;
+    "waybar/style.css".source = ./waybar.style.css;
   };
 }
