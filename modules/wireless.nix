f1e5a2c8105470a3e62b01ec671042572f10b5ee
derui <derutakayu@gmail.com>
2024-10-16 22:22:55 +0900

    fix: remove wireless

--- modules/wireless.nix
+++ modules/wireless.nix
@@ -2,20 +2,14 @@
   # 無線LANを設定するための処理をNetworkManager経由で行う
   networking.networkmanager = {
     enable = true;
-
     insertNameservers = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
-
     dns = "dnsmasq";
   };
 
-  networking.supplicant = {
-    "wlp8s0" = {
-      configFile.path = "/etc/nixos/secrets/wpa_supplicant.conf";
-      extraConf = ''
-        ap_scan=1
-        p2p_disabled=1
-      '';
+  services.dnsmasq = {
+    enable = true;
+    settings = {
+      server = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
     };
   };
-
 }
