e3ba1a9830476ec0a888f9f2faeb0c581a8ff5ef
derui <derutakayu@gmail.com>
2024-10-17 21:39:07 +0900

    fix: do not use networkmanager on nixos

--- modules/wireless.nix
+++ modules/wireless.nix
@@ -1,15 +1,11 @@
+{ config, ... }:
 {
-  # 無線LANを設定するための処理をNetworkManager経由で行う
-  networking.networkmanager = {
-    enable = true;
-    insertNameservers = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
-    dns = "dnsmasq";
-  };
+  networking.nameservers = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
 
   services.dnsmasq = {
     enable = true;
     settings = {
-      server = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
+      server = config.networking.nameservers;
     };
   };
 }
