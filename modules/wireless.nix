b0936f6f5a06476e337defbb3bb989e205afd504
derui <derutakayu@gmail.com>
2024-10-21 20:35:00 +0900

    feat: use systemd-resolved for DNS resolution

--- modules/wireless.nix
+++ modules/wireless.nix
@@ -1,4 +1,17 @@
 { config, ... }:
 {
   networking.nameservers = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
+
+  # nscdはそもそもdeprecatedということなので利用しない
+  services.nscd = {
+    enable = true;
+  };
+
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
+    dnsovertls = "true";
+  };
 }
