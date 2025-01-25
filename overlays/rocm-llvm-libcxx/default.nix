8eae0a6073746c653b331e3e5ed11caa49cdf02f
derui <derutakayu@gmail.com>
2025-01-25 18:42:22 +0900

    fix: use final to avoid evaluation error

--- overlays/rocm-llvm-libcxx/default.nix
+++ overlays/rocm-llvm-libcxx/default.nix
@@ -1,6 +1,6 @@
 # TODO https://github.com/NixOS/nixpkgs/pull/375850 がunstableにmergeされたら消す
 final: prev: {
-  rocmPackages.llvm.libcxx = prev.rocmPackages.llvm.libcxx.overrideAttrs (old: {
+  final.rocmPackages.llvm.libcxx = prev.rocmPackages.llvm.libcxx.overrideAttrs (old: {
     # Most of these can't find `bash` or `mkdir`, might just be hard-coded paths, or PATH is altered
     extraPostPatch = ''
       chmod +w -R ../libcxx/test/{libcxx,std}
