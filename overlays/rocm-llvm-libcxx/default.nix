fb5a9c0bafb56e61dac550d3a7281e1b6010e369
derui <derutakayu@gmail.com>
2025-01-25 19:10:31 +0900

    fix: use overrideDeriviation

--- overlays/rocm-llvm-libcxx/default.nix
+++ overlays/rocm-llvm-libcxx/default.nix
@@ -1,10 +1,27 @@
 # TODO https://github.com/NixOS/nixpkgs/pull/375850 がunstableにmergeされたら消す
 final: prev: {
-  final.rocmPackages.llvm.libcxx = prev.rocmPackages.llvm.libcxx.overrideAttrs (old: {
-    # Most of these can't find `bash` or `mkdir`, might just be hard-coded paths, or PATH is altered
-    extraPostPatch = ''
-      chmod +w -R ../libcxx/test/{libcxx,std}
-      cat ${./1000-libcxx-failing-tests.list} | xargs -d \\n rm
-    '';
-  });
+  rocmPackages = prev.rocmPackages // {
+    llvm = prev.rocmPackages.llvm // {
+      libcxx = prev.rocmPackages.llvm.libcxx.overrideAttrs (
+        { targetDir }:
+        {
+          # Most of these can't find `bash` or `mkdir`, might just be hard-coded paths, or PATH is altered
+          postPatch = ''
+            cd ${targetDir}
+
+            patchShebangs lib/OffloadArch/make_generated_offload_arch_h.sh
+
+            # FileSystem permissions tests fail with various special bits
+            rm test/tools/llvm-objcopy/ELF/mirror-permissions-unix.test
+            rm unittests/Support/Path.cpp
+
+            substituteInPlace unittests/Support/CMakeLists.txt \
+              --replace-fail "Path.cpp" ""
+                chmod +w -R ../libcxx/test/{libcxx,std}
+                cat ${./1000-libcxx-failing-tests.list} | xargs -d \\n rm
+          '';
+        }
+      );
+    };
+  };
 }
