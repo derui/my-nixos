027e07a63f7dce06ebd611aad6a608c73261c5c2
derui <derutakayu@gmail.com>
2025-01-25 19:35:09 +0900

    fix: use override

--- overlays/rocm-llvm-libcxx/default.nix
+++ overlays/rocm-llvm-libcxx/default.nix
@@ -2,26 +2,13 @@
 final: prev: {
   rocmPackages = prev.rocmPackages // {
     llvm = prev.rocmPackages.llvm // {
-      libcxx = prev.rocmPackages.llvm.libcxx.overrideAttrs (
-        { targetDir }:
-        {
-          # Most of these can't find `bash` or `mkdir`, might just be hard-coded paths, or PATH is altered
-          postPatch = ''
-            cd ${targetDir}
-
-            patchShebangs lib/OffloadArch/make_generated_offload_arch_h.sh
-
-            # FileSystem permissions tests fail with various special bits
-            rm test/tools/llvm-objcopy/ELF/mirror-permissions-unix.test
-            rm unittests/Support/Path.cpp
-
-            substituteInPlace unittests/Support/CMakeLists.txt \
-              --replace-fail "Path.cpp" ""
-                chmod +w -R ../libcxx/test/{libcxx,std}
-                cat ${./1000-libcxx-failing-tests.list} | xargs -d \\n rm
-          '';
-        }
-      );
+      libcxx = prev.rocmPackages.llvm.libcxx.overrideDerivation (oldAttrs: {
+        # Most of these can't find `bash` or `mkdir`, might just be hard-coded paths, or PATH is altered
+        postPatch = ''
+          ${oldAttrs.postPatch}
+          cat ${./1000-libcxx-failing-tests.list} | xargs -d \\n rm
+        '';
+      });
     };
   };
 }
