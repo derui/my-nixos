c2f16402e5757310ed051a20290b8be65040066a
derui <derutakayu@gmail.com>
2026-01-10 12:47:50 +0900

    fix: some configuration changes

--- home-manager/gui/firefox.nix
+++ home-manager/gui/firefox.nix
@@ -18,9 +18,9 @@
         in
         # uuidは about:debugging#/runtime/this-firefox から取得できる。
         listToAttrs [
-          (extension "tree-style-tab" "treestyletab@piro.sakura.ne.jp")
           (extension "vimium" "{d7742d87-e61d-4b78-b8a1-b469842139fa}")
           (extension "bitwarden-password-manager" "{446900e4-71c2-419f-a6a7-df9c091e268b}")
+          (extension "ublock origin" "{67b59983-7d7c-482e-8e20-6bd27ab6d5d9}")
         ];
 
       HardwareAcceleration = true;
@@ -34,6 +34,23 @@
         # Performance settings
         "gfx.webrender.all" = true; # Force enable GPU acceleration
         "media.ffmpeg.vaapi.enabled" = true;
+        "datareporting.policy.dataSubmissionEnabled" = false;
+        "datareporting.healthreport.uploadEnabled" = false;
+        "toolkit.telemetry.unified" = false;
+        "toolkit.telemetry.enabled" = false;
+        "toolkit.telemetry.server" = "data: = ";
+        "toolkit.telemetry.archive.enabled" = false;
+        "toolkit.telemetry.newProfilePing.enabled" = false;
+        "toolkit.telemetry.shutdownPingSender.enabled" = false;
+        "toolkit.telemetry.updatePing.enabled" = false;
+        "toolkit.telemetry.bhrPing.enabled" = false;
+        "toolkit.telemetry.firstShutdownPing.enabled" = false;
+        "toolkit.telemetry.coverage.opt-out" = true;
+        "toolkit.coverage.opt-out" = true;
+        "toolkit.coverage.endpoint.base" = "";
+        "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+        "browser.newtabpage.activity-stream.telemetry" = false;
+        "datareporting.usage.uploadEnabled" = false;
       };
 
       search.force = true;
