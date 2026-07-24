8033512241d109f376757ed9942e5806fddf810b
derui <derutakayu@gmail.com>
2026-07-24 23:21:44 +0900

    Update llama-cpp.nix

--- modules/llama-cpp.nix
+++ modules/llama-cpp.nix
@@ -24,6 +24,9 @@ in
         "gemma-4-nt:12b" = {
           cmd = "${llama-server} -dev ROCm0 -hf unsloth/gemma-4-12b-it-GGUF:Q4_K_S --port \${PORT} --n-gpu-layers 999 --repeat-penalty 1.0 --reasoning off --spec-type draft-mtp --spec-draft-n-max 2";
         };
+        "gemma-4-it:12b" = {
+          cmd = "${llama-server} -dev ROCm0 -hf unsloth/gemma-4-12b-it-GGUF:Q4_K_S --port \${PORT} --n-gpu-layers 999 --repeat-penalty 1.0 --reasoning on --spec-type draft-mtp --spec-draft-n-max 2";
+        };
         "qwen3.6:27b" = {
           cmd = "${llama-server} -dev ROCm0 -hf unsloth/Qwen3.6-27B-MTP-GGUF:IQ4_XS --port \${PORT} \\
              -ngl 99 -c 8192 -fa on -np 1 \\
