f33168ebaad6cb506462d92e288aed80e144b957
derui <derutakayu@gmail.com>
2026-08-18 21:54:44 +0900

    chore: update flake.lock and github-copilot-cli version

--- pkgs/github-copilot-cli.nix
+++ pkgs/github-copilot-cli.nix
@@ -26,7 +26,7 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "github-copilot-cli";
-  version = "1.0.76";
+  version = "1.0.80";
 
   src = fetchurl {
     url = "https://github.com/github/copilot-cli/releases/download/v${finalAttrs.version}/github-copilot-${finalAttrs.version}-${platform}.tgz";
@@ -34,7 +34,7 @@ stdenv.mkDerivation (finalAttrs: {
       {
         "x86_64-darwin" = "sha256-D72R1Vt/6eSg7INVYjPtC5W/6oPVzpVC1Tn4q831Wqs=";
         "aarch64-darwin" = "sha256-Ly/Tay3iOMzsipaWLTTh3HKBYwvq7Nu3yQpYrC39UPI=";
-        "x86_64-linux" = "sha256-sr/kUeP54o1SXKvfyvpJYASPH44IBxSI3WQ8Nu+t+hc=";
+        "x86_64-linux" = "sha256-2s6jYMgj3Vj3ENeoHDa76fZ3PfRYkIeqJctmumN8gig=";
         "aarch64-linux" = "sha256-3l260k1Uw79owiBP2bhNfGgqkE35JN7zPSb8OXIpeuI=";
       }
       .${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
